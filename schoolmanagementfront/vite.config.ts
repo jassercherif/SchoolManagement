9e3e2516851220c224c0b86fbb38e02b4da4dfe1
jassercherif <jasser.cherif@ensi-uma.tn>
2024-10-28 13:40:36 +0100

    vite configuration

--- schoolmanagementfront/vite.config.ts
+++ schoolmanagementfront/vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: '0.0.0.0', // This will expose the server to all network interfaces
+    port: 5173,      // Ensure the port is set to 5173
+  },
+});
